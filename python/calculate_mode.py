3a28855bb8552e380866c89046eb47ad43c70628
Kate Kim <katehrkim@gmail.com>
2021-05-26 22:24:15 -0400

    Kate's answer

--- python/calculate_mode.py
+++ python/calculate_mode.py
@@ -1,2 +1,17 @@
-def calculate_mode():
-    pass
+def calculate_mode(numbers):
+    dictionary = {}
+    for number in numbers:
+        if number in dictionary:
+            dictionary[number] = dictionary[number] + 1
+        else:
+            dictionary[number] = 1
+    max = 0
+    output = []
+    for key in dictionary:
+        if dictionary[key] == max:
+            output.append(key)
+        elif dictionary[key] > max:
+            max = dictionary[key]
+            output.clear()
+            output.append(key)
+    return output
--- python/calculate_mode_spec.py
+++ python/calculate_mode_spec.py
@@ -1,1 +1,22 @@
 # Write your own unit tests here!
+import unittest
+from calculate_mode import calculate_mode
+
+class CalculateModeTestCase(unittest.TestCase):
+    """Tests for calculate_mode.py"""
+    def test_one(self):
+        self.assertEqual(calculate_mode([1,2,3,3]), [3])
+    def test_two(self):
+        self.assertEqual(calculate_mode([4.5, 0, 0]), [0])
+    def test_three(self):
+        self.assertEqual(calculate_mode([1.5, -1, 1, 1.5]), [1.5])
+    def test_four(self):
+        self.assertEqual(calculate_mode([1,1,2,2]), [1,2])
+    def test_five(self):
+        self.assertEqual(calculate_mode([1,2,3]), [1,2,3])
+    def test_six(self):
+        self.assertEqual(calculate_mode(["who", "what", "where", "who"]), ["who"])
+    
+
+if __name__ == '__main__':
+    unittest.main()
